d87cdcb36282b62034f753609f8d48e8dae000a8
AshutoshDas <ashutosh.rkl.2001@gmail.com>
2024-01-01 23:44:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "./Pages/Router/Router";
+
+const App = () => {
+  return (
+    <div>
+
+      <Router/>
+      
+    </div>
+  );
+};
+
+export default App;
